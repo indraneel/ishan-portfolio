f3c32f10e52b0866757c2c1101c9c09caac5a964
Indraneel Purohit <i.m.indraneel@gmail.com>
2024-03-23 19:24:24 -0400

    Update fetch_airtable_data.py

--- fetch_airtable_data.py
+++ fetch_airtable_data.py
@@ -10,6 +10,7 @@ import mimetypes
 
 AIRTABLE_API_KEY = os.environ.get("AIRTABLE_API_KEY")
 AIRTABLE_BASE_ID = os.environ.get("AIRTABLE_BASE_ID")
+print(AIRTABLE_API_KEY, AIRTABLE_BASE_ID)
 
 api = Api(api_key=AIRTABLE_API_KEY)
 table = api.table(AIRTABLE_BASE_ID, table_name='Pages')
